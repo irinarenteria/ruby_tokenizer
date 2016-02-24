67c50e21655b5d5b12ab743cf29f51b9bb50ea5a
irinarenteria <renteria.irina@gmail.com>
2016-02-24 14:53:38 -0600

    Fix indendation attempt

--- lib/patterns_module/patterns.rb
+++ lib/patterns_module/patterns.rb
@@ -1,9 +1,7 @@
 module Patterns
-
-	PUNCTUATION = [':', ',', '—', '!', '?', ';', '.', '"']
-	PARENS = ['{', '[', '}', ']', '(', ')']
-	MISC = ['<', '«', '„', '>', '»', '“', '*']
-	CONJUNCTIONS = ['for ', 'and ', 'nor ', 'but ', 'or ', 'yet ', 'so ', 'as ']
-	DETERMINERS = ['the ', 'this ', 'that ', 'i ']
-	
+  PUNCTUATION = [':', ',', '—', '!', '?', ';', '.', '"']
+  PARENS = ['{', '[', '}', ']', '(', ')']
+  MISC = ['<', '«', '„', '>', '»', '“', '*']
+  CONJUNCTIONS = ['for ', 'and ', 'nor ', 'but ', 'or ', 'yet ', 'so ', 'as ']
+  DETERMINERS = ['the ', 'this ', 'that ', 'i ']
 end
