68b601b5b7c2e9a7bf3d9dc73af7250cf25d44b6
irinarenteria <renteria.irina@gmail.com>
2016-02-24 00:46:54 -0600

    Add helper method to spec

--- spec/ruby_tokenizer_spec.rb
+++ spec/ruby_tokenizer_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+require 'ruby_tokenizer'
+
 describe RubyTokenizer do
+	let(:token) {RubyTokenizer.new('Searching records is a common requirement in web applications.')}
+  
   it 'has a version number' do
     expect(RubyTokenizer::VERSION).not_to be nil
   end
