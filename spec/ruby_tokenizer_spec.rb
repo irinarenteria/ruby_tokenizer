7a27cca6a3e49a2c6c9641fe44dd5d567f27a714
irinarenteria <renteria.irina@gmail.com>
2016-02-25 16:25:27 -0600

    Fix indentation and remove trailing whitespaces

--- spec/ruby_tokenizer_spec.rb
+++ spec/ruby_tokenizer_spec.rb
@@ -2,27 +2,26 @@ require 'spec_helper'
 require 'ruby_tokenizer'
 
 describe RubyTokenizer do
-	let(:token) {RubyTokenizer::Tokenizer.new('Searching records is a common requirement in web applications.')}
+  let(:token) {RubyTokenizer::Tokenizer.new('Searching records is a common requirement in web applications.')}
   RubyTokenizer::Tokenizer.send(:public, *RubyTokenizer::Tokenizer.protected_instance_methods) 
 
-	describe "#version" do	
-		it 'has a version number' do
-			expect(RubyTokenizer::VERSION).not_to be nil
-  		end
-	end
+  describe "#version" do	
+    it 'has a version number' do
+      expect(RubyTokenizer::VERSION).not_to be nil
+    end
+  end
 
-  	describe "#new" do
-		it "takes one parameter and returns a Tokenizer object" do
-			expect(token).to be_an_instance_of RubyTokenizer::Tokenizer
-		end
+  describe "#new" do
+    it "takes one parameter and returns a Tokenizer object" do
+      expect(token).to be_an_instance_of RubyTokenizer::Tokenizer
+    end
 
-		it "contains a String" do
-			expect(token.text).to be == 'Searching records is a common requirement in web applications.'
-		end
-	end
-
+    it "contains a String" do
+      expect(token.text).to be == 'Searching records is a common requirement in web applications.'
+    end
+  end
+  
   describe "#filter" do
-
     it "returns a String" do
       expect(token.filter).to be_a_kind_of String
     end
