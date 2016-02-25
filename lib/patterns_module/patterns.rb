263aef0b35efb313ad3f1ff6195ff5455ed500cd
irinarenteria <renteria.irina@gmail.com>
2016-02-24 23:19:49 -0600

    Disable determiner and conjunctions patterns for now

--- lib/patterns_module/patterns.rb
+++ lib/patterns_module/patterns.rb
@@ -2,10 +2,10 @@ module Patterns
   PUNCTUATION = [':', ',', '—', '!', '?', ';', '.', '"']
   PARENS = ['{', '[', '}', ']', '(', ')']
   MISC = ['<', '«', '„', '>', '»', '“', '*']
-  CONJUNCTIONS = ['for ', 'and ', 'nor ', 'but ', 'or ', 'yet ', 'so ', 'as ']
-  DETERMINERS = ['the ', 'this ', 'that ']
+  # CONJUNCTIONS = ['for ', 'and ', 'nor ', 'but ', 'or ', 'yet ', 'so ', 'as ']
+  # DETERMINERS = ['the ', 'this ', 'that ']
 
-  BASIC = PUNCTUATION + MISC + DETERMINERS + CONJUNCTIONS
+  BASIC = PUNCTUATION + MISC
 
   def self.basic
     Regexp.union(BASIC)
