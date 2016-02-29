04c64e91523012e7a1b2748a1ef82f9cb4c95d0f
irinarenteria <renteria.irina@gmail.com>
2016-02-29 12:36:20 -0600

    Add ARGV as param to analyze method

--- lib/input_stream_module/input_stream.rb
+++ lib/input_stream_module/input_stream.rb
@@ -1,15 +1,14 @@
 module InputStream
 
-  def self.analyze(*input)
+  def self.analyze(input)
     if input.empty?
       puts "--- Please input your text below ----"
       text = STDIN.gets
-    elsif input.detect { |input| File.file?(input) == false }
+    elsif input.detect { |path| File.file?(path) == false }
       puts "File not found: Please try again."
       exit 2
     else
       text = ARGF.read
     end
   end
-
 end
--- spec/input_stream_spec.rb
+++ spec/input_stream_spec.rb
@@ -2,17 +2,16 @@ require 'spec_helper'
 require 'input_stream_module/input_stream'
 
 describe InputStream do
-
+  
   describe ".analyze" do
     context "when the input is empty" do
       pending
     end
-
     context "when the input is a file path that can't be found" do
       pending
     end
 
-    context "when the input is a file path that can be read" do
+    context "when the input is a file that can be read" do
       pending
     end
   end
