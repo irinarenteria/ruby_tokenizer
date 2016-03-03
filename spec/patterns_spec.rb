b029073f04f49bca8db7f0d739947e45f7601503
irinarenteria <renteria.irina@gmail.com>
2016-03-03 13:08:44 -0600

    Write tests for constants in pattern module

--- spec/patterns_spec.rb
+++ spec/patterns_spec.rb
@@ -3,6 +3,12 @@ require 'patterns_module/patterns'
 
 describe Patterns do
 
+  describe "constants" do
+    it "contains patterns as constants" do
+      expect(Patterns::PUNCTUATION).to eq([':', ',', '—', '!', '?', ';', '"'])
+    end
+  end
+
   describe "#basic" do
     it "should return a Regexp object" do
       expect(Patterns.basic).to be_a_kind_of Regexp
