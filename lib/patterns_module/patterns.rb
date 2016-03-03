5180d9eead01cefed0885492c68d2145eca5f200
irinarenteria <renteria.irina@gmail.com>
2016-03-03 13:15:57 -0600

    Write tests for rest of constants and basic tokenizing pattern

--- lib/patterns_module/patterns.rb
+++ lib/patterns_module/patterns.rb
@@ -5,7 +5,7 @@ module Patterns
   # CONJUNCTIONS = ['for ', 'and ', 'nor ', 'but ', 'or ', 'yet ', 'so ', 'as ']
   # DETERMINERS = ['the ', 'this ', 'that ']
 
-  BASIC = PUNCTUATION + MISC
+  BASIC = PUNCTUATION + PARENS + MISC
 
   def self.basic
     Regexp.union(BASIC)
--- spec/patterns_spec.rb
+++ spec/patterns_spec.rb
@@ -4,8 +4,20 @@ require 'patterns_module/patterns'
 describe Patterns do
 
   describe "constants" do
-    it "contains patterns as constants" do
+    it "contains punctuation as a constant" do
       expect(Patterns::PUNCTUATION).to eq([':', ',', '—', '!', '?', ';', '"'])
+    end
+
+    it "contains parens as a constant" do
+      expect(Patterns::PARENS).to eq(['{', '[', '}', ']', '(', ')'])
+    end
+
+    it "contains misc. characters as a constant" do
+      expect(Patterns::MISC).to eq(['<', '«', '„', '>', '»', '“', '*'])
+    end
+
+    it "contains a low-level tokenizing pattern" do
+      expect(Patterns::BASIC).to eq(Patterns::PUNCTUATION + Patterns::PARENS + Patterns::MISC)
     end
   end
 
