80d6ee7e2f84285d9fae7d6509ae5dbba53bae90
irinarenteria <renteria.irina@gmail.com>
2016-03-03 12:45:09 -0600

    Write test for ARGF

--- spec/input_stream_spec.rb
+++ spec/input_stream_spec.rb
@@ -22,9 +22,24 @@ describe InputStream do
 
   describe ".read_file" do
     let(:file) { InputStream.read_file(['no.txt']) }
+    let(:content) { 'This is a test.' }
+    let(:test_file) { Tempfile.new("test") }
 
     it "raises an error when the file can't be found or read" do
       expect { file }.to raise_error(LoadError, "File not found: Please try again.")
+    end
+
+    it "reads input files" do
+    mod_double = class_double("InputStream").as_stubbed_const(:transfer_nested_constants => true)
+    allow(mod_double).to receive(:read_file).with(kind_of(Array))
+
+    test_file.write(content)
+    test_file.rewind
+
+    expect { mod_double.read_file([test_file.path]) }.to output(kind_of(String)).to_stdout
+
+    test_file.close
+    test_file.unlink
     end
   end
 end
