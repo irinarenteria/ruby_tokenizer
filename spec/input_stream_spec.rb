135e5bb33ce5e53944f2ea23a63c11822c3ec7ce
irinarenteria <renteria.irina@gmail.com>
2016-03-03 12:41:06 -0600

    Write test for user prompt method

--- spec/input_stream_spec.rb
+++ spec/input_stream_spec.rb
@@ -4,7 +4,11 @@ require 'input_stream_module/input_stream'
 describe InputStream do
   
   describe ".prompt" do
-    pending
+    let(:string_output) { InputStream.prompt("--- Please input your text below ----") }
+
+    it "takes a string as a parameter and outputs to STDOUT" do
+      expect { string_output }.to output("--- Please input your text below ----\n").to_stdout
+    end
   end
 
   describe ".get_input" do
